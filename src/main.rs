880fd701fc407a25c5029bf9606bc469e5bb0171
politox17 <politoandrea74@gmail.com>
2025-09-11 15:31:21 +0200

    Creazione dell'interfaccia con le scelte proposte

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 
 use registro_persone::db::menu;
+use crate db::menu::leggi_input_int;
 
 
 
@@ -8,5 +9,16 @@ use registro_persone::db::menu;
 fn main() {
     println!("\t\tBenvenuto nella nostra pagina! Leggi e scegli cosa fare!");
     println!("=========================================================");
+    println!("[1] Iscrizione torneo calcio");
+    println!("[2] Mostra iscritti");
+    println!("[3] Cerca per nome");
+    println!("[4] Cerca per cognome");
+    println!("[5] Aggiorna");
+    println!("[6] Rimuovi");
+    println!("[7] Esci\n");
+    println!("Selezionare il numero: \n");
+    let mut input = leggi_input_int();
     
+
+
 }
