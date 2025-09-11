e6366536dbc891f252b53c2a0e347c0700d23696
politox17 <politoandrea74@gmail.com>
2025-09-11 09:50:54 +0200

    Aggiunga delle 3 funzioni new(), aggiungi_iscritti(), creazione() come metodi di Registro

--- src/db/registro.rs
+++ src/db/registro.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use rand::Rng;
 
 #[derive(Debug)]
 pub struct Registro{
@@ -9,4 +10,19 @@ pub struct Registro{
 pub struct Iscritto {
     nome: String,
     cognome: String,
+}
+
+impl Registro {
+    pub fn new() -> Self {
+        iscritti::HashMap::new(),
+    }
+    pub fn aggiungi_iscritti(&mut self, nome: String) -> i32 {
+           let id = Self::creazione_id();
+           self.iscritti.insert(id, nome.to_string());
+    }
+    pub fn creazione_id() -> i32 {
+        let mut random = rand::rng();
+        let id = random.random_range(100..999);
+        id
+    }
 }
